7ebc691725c5fbcfea00b2ac9ac1405e93e6eee1
Johnny J <johnnyj@nvidia.com>
2025-07-19 23:09:10 +0800

    min privileges and best show

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pytest-actions-report-demo",
+    name="pytest_actions_report_demo",
     version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
+    package_dir={"": "src"},  # 关键点：指定源码在src目录
     install_requires=[],
 )
