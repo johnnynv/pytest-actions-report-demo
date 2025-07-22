0a8edde56b59eaf71efa69fde2ba54d961ca9fb4
Johnny J <johnnyj@nvidia.com>
2025-07-22 10:49:18 +0800

    add .pre-commit

--- src/pytestshow/__init__.py
+++ src/pytestshow/__init__.py
@@ -1,6 +1,7 @@
+# This is NVIDIA proprietary code and confidential. All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
 # src/pytestshow/__init__.py
 __version__ = "0.1.0"  # 与setup.py/pyproject.toml保持一致
 
-# 可选：显式导出主要功能
 from .math_ops import *
 from .string_ops import *
